2eff13ee1be50bd56df8780463aeff027a0a9057
haritha <haritharavichandran11@gmail.com>
2026-03-16 21:57:58 +0530

    dependency of mysql included and chnges in loginmodule

--- college-management-system/src/main/java/controller/LoginController.java
+++ college-management-system/src/main/java/controller/LoginController.java
@@ -3,33 +3,29 @@ package controller;
 import service.AuthService;
 
 public class LoginController {
-	AuthService authservice=new AuthService();
 
-	public void login(String email, String password) {
-		String role=authservice.checkrole(email,password);
-		
-		 if(role.equals("ADMIN")) {
-	            System.out.println("Admin Login Successful");
-	            AdminController adminController = new AdminController();
-	            adminController.showMenu();
-	        }
+    private AuthService authService = new AuthService();
 
-		 else if(role.equals("FACULTY")) {
-			    System.out.println("Faculty Login Successful");
-			    FacultyController facultyController = new FacultyController();
-			    facultyController.showMenu();
-			}
+    public void login(String email, String password) {
 
-	        else if(role.equals("STUDENT")) {
-	            System.out.println("Student Login Successful");
-	            StudentController studentController = new StudentController();
-	            studentController.showMenu();
-	        }
+        String role = authService.checkRole(email, password);
 
-	        else {
-	            System.out.println("Invalid Login Credentials");
-	        }
-		
-	}
-	
+        if (role == null) {
+            System.out.println("Invalid Login Credentials");
+            return;
+        }
+
+        if ("ADMIN".equals(role)) {
+            System.out.println("Admin Login Successful");
+            new AdminController().showMenu();
+        } 
+        else if ("FACULTY".equals(role)) {
+            System.out.println("Faculty Login Successful");
+            new FacultyController().showMenu();
+        } 
+        else if ("STUDENT".equals(role)) {
+            System.out.println("Student Login Successful");
+            new StudentController().showMenu();
+        }
+    }
 }
--- college-management-system/src/main/java/service/AuthService.java
+++ college-management-system/src/main/java/service/AuthService.java
@@ -3,10 +3,10 @@ package service;
 import dao.UserDAO;
 
 public class AuthService {
-	UserDAO user_dao=new UserDAO();
-	
-	public String checkrole(String email,String password) {
-		 return user_dao.checkUser(email,password);
-	}
 
+    private UserDAO userDAO = new UserDAO();
+
+    public String checkRole(String email, String password) {
+        return userDAO.checkUser(email, password);
+    }
 }
